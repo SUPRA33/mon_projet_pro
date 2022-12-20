63e438036edba81a9aa2d87a2f0d950280dcac7d
Romain Bertaud <bertaud.romain@hotmail.fr>
2022-12-20 17:59:37 +0100

    add nodemailer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,38 +1,47 @@
 import banner from "../assets/img/banner_contact.jpg"
 import Header from "./Header";
 import Footer from "./Footer";
+import { useState } from "react";
 
 const Contact = () => {
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState(false);
 
-        console.log(event.target.mail.value);
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-        const last_name = event.target.last_name.value;
-        const first_name = event.target.first_name.value;
-        const email = event.target.mail.value;
-        const message_object = event.target.objSelectOption.value;
-        const message = event.target.message_send.value;
+        const last_name = e.target.last_name.value;
+        const first_name = e.target.first_name.value;
+        const phone = e.target.phone.value;
+        const email = e.target.email.value;
+        const message_object = e.target.message_object.value;
+        const message = e.target.message_send.value;
 
-        const jwtLocalStorage = localStorage.getItem('jwt');
-        const token = JSON.parse(jwtLocalStorage).access_token;
+        if (firstName.length == 0 || lastName.length == 0 || email.length == 0 || message.length == 0) {
+            setError(true);
+        }
+        if (last_name && first_name && email && message) {
+            alert("Message envoyé avec succès.")
+        }
 
         fetch('http://localhost/api/contact', {
-            method: 'PUT',
+            method: 'POST',
             headers: {
-                authorization: 'Bearer ' + token,
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 last_name,
                 first_name,
+                phone,
                 email,
                 message_object,
                 message
             })
         });
-        
     }
 
     return(
@@ -49,15 +58,19 @@ const Contact = () => {
                             <div className="info">
                                 <div className="content">
                                     <label htmlFor="last_name">Nom*</label>
-                                    <input type="text" name="last_name"/>
+                                    <input type="text" name="last_name" placeholder="Nom" onChange={e=>setLastName(e.target.value)}/>
+                                    {error && lastName.length <= 0 ?
+                                    <label className="error">Nom ne peut être vide</label>:""}
                                 </div>
                                 <div className="content">
-                                    <label htmlFor="mail">Email*</label>
-                                    <input type="email" name="mail"/>
+                                    <label htmlFor="email">Email*</label>
+                                    <input type="email" name="email" placeholder="utilisateur@gmail.com" onChange={e=>setEmail(e.target.value)}/>
+                                    {error && email.length <= 0  ?
+                                    <label className="error">Email ne peut être vide</label>:""}
                                 </div>
                                 <div className="content">
                                     <label htmlFor="object">Objet*</label>
-                                    <select name="objSelectOption">
+                                    <select name="message_object">
                                         <option value="" id="chooseObject">-- Choisissez un objet --</option>
                                         <option>Boutique</option>
                                         <option>Recrutement</option>
@@ -69,17 +82,21 @@ const Contact = () => {
                             <div className="info">
                                 <div className="content">
                                     <label htmlFor="first_name">Prénom*</label>
-                                    <input type="text" name="first_name"/>
+                                    <input type="text" name="first_name" placeholder="Prénom" onChange={e=>setFirstName(e.target.value)}/>
+                                    {error && firstName.length <= 0  ?
+                                    <label className="error">Prénom ne peut être vide</label>:""}
                                 </div>
                                 <div className="content">
-                                    <label htmlFor="number">Téléphone</label>
-                                    <input type="tel" id="number" name="number"/>
+                                    <label htmlFor="phone">Téléphone</label>
+                                    <input type="tel" name="phone" placeholder="06372869XX"/>
                                 </div>
                             </div>
                         </div>
                         <div className="message">
                             <label htmlFor="message">Message*</label>
-                            <textarea name="message_send" cols="80" rows="8"></textarea>
+                            <textarea name="message_send" cols="80" rows="8" placeholder="Veuillez saisir votre demande." onChange={e=>setMessage(e.target.value)}></textarea>
+                            {error && message.length <= 0  ?
+                            <label className="error">Message ne peut être vide</label>:""}
                         </div>
                         <div>
                             <button role="button">Envoyer</button>
